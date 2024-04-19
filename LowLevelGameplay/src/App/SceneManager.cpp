c118fa85027e2eb2735243270a66df4e9bb7aefa
Robyn <92083124+multidimensionalsock@users.noreply.github.com>
2024-04-19 12:04:40 +0100

    still dk if the maths works

--- LowLevelGameplay/src/App/SceneManager.cpp
+++ LowLevelGameplay/src/App/SceneManager.cpp
@@ -34,27 +34,36 @@ void LLGP::SceneManager::CheckCollisions()
 {
 	//go throught the collider list and check if theyre colliding 
 	//tell the object the collisions status 
+	bool col = false;
 	for (int i = 0; i < _SceneColliders.size(); i++)
 	{
+		
 		for (int j = 0; j < _SceneColliders.size(); j++)
 		{
-			if (_SceneColliders[i]->type == AABB && _SceneColliders[i]->type == AABB)
+			col = false;
+			if (_SceneColliders[i]->type == _SceneColliders[j]->type)
 			{
-				_SceneColliders[i]->AABBAABBCollision(static_cast<AABBCollider*>(_SceneColliders[i]), static_cast<AABBCollider*>(_SceneColliders[j]));
-			}
-			else if (_SceneColliders[i]->type == Circle && _SceneColliders[i]->type == AABB)
-			{
-				_SceneColliders[i]->CircleAABBCollision(static_cast<CircleCollider*>(_SceneColliders[i]), static_cast<AABBCollider*>(_SceneColliders[j]));
-			}
-			else if (_SceneColliders[i]->type == AABB && _SceneColliders[i]->type == Circle)
-			{
-				_SceneColliders[i]->CircleAABBCollision(static_cast<CircleCollider*>(_SceneColliders[j]), static_cast<AABBCollider*>(_SceneColliders[i]));
+				if (_SceneColliders[i]->type == AABB)
+				{
+					col = _SceneColliders[i]->AABBAABBCollision(static_cast<AABBCollider*>(_SceneColliders[i]), static_cast<AABBCollider*>(_SceneColliders[j]));
+				}
+				else
+				{
+					col = _SceneColliders[i]->CircleCircleCollision(static_cast<CircleCollider*>(_SceneColliders[i]), static_cast<CircleCollider*>(_SceneColliders[j]));
+				}
 			}
 			else 
 			{
-				_SceneColliders[i]->CircleCircleCollision(static_cast<CircleCollider*>(_SceneColliders[i]), static_cast<CircleCollider*>(_SceneColliders[j]));
-
+				if (_SceneColliders[i]->type == AABB)
+				{
+					col = _SceneColliders[i]->CircleAABBCollision(static_cast<CircleCollider*>(_SceneColliders[j]), static_cast<AABBCollider*>(_SceneColliders[i]));
+				}
+				else
+				{
+					col = _SceneColliders[i]->CircleAABBCollision(static_cast<CircleCollider*>(_SceneColliders[i]), static_cast<AABBCollider*>(_SceneColliders[j]));
+				}
 			}
+			_SceneColliders[i]->GetGameObject()->Colliding(col, _SceneColliders[j]->GetGameObject());
 		}
 	}
 }
