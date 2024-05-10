dcf8fad201ddab36b740fae9c93a8c767eb8ca4f
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2024-05-10 16:19:13 +0100

    spawn positions

--- LowLevelGameplay/src/App/Bounder.cpp
+++ LowLevelGameplay/src/App/Bounder.cpp
@@ -2,13 +2,25 @@
 #include "Core/Renderer.h"
 #include "Core/ColliderTypes.h"
 #include "Core/BounderAI.h"
+#include <cstdlib>
 
 void LLGP::Bounder::Start()
 {
 	SetTag("Bounder");
 
 	TextureDetails tex = sceneManager->textureLoad.enemyBird;
-	transform.position = Vector2<float>(100, 100); //do this so it appears at a random spawn point 
+
+	//spawner positions
+	Vector2<float> positions[4] = 
+	{ 
+		Vector2<float>(250.f  ,146.f - tex.RectTexSize.y / 2),
+		Vector2<float>(52.f ,262.f - tex.RectTexSize.y / 2),
+		Vector2<float>(510.f ,240.f - tex.RectTexSize.y / 2),
+		Vector2<float>(282.f ,407.f - tex.RectTexSize.y / 2)
+	};
+	Vector2<float> pos = positions[std::rand() % 4];
+
+	transform.position = pos; //do this so it appears at a random spawn point 
 
 	//needs a renderer
 	Renderer* ren = AddComponent<Renderer>();
